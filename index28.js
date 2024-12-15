b3511b5e7a0bc79c2344b90e47b15824cdd6354c
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2024-12-15 13:14:43 +0500

    JsActivity1-Q28

---
+++ index28.js
@@ -0,0 +1,9 @@
+import { name, age, salary } from "./index27.js";
+
+let info = () => {
+  console.log(`Name is ${name}`);
+  console.log(`Age is ${age}`);
+  console.log(`Salary is ${salary}`);
+};
+
+info();
