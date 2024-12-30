b2b492e75a0c7d5ca543441664bf5d2050283eab
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2024-12-30 20:29:42 +0500

    JsActivity1-Q52

---
+++ index52.js
@@ -0,0 +1,17 @@
+let promo = (complete, name) => {
+  return new Promise((resolve, reject) => {
+    console.log("Please wait fetching data...");
+    setTimeout(() => {
+      if (complete) {
+        resolve(`Username is ${name}`);
+      } else {
+        reject("Promo is not running");
+      }
+    }, 2000);
+  });
+};
+(async () => {
+  console.log(await promo(true, "Yasir"));
+  console.log(await promo(true, "Ali"));
+  console.log(await promo(true, "Asad"));
+})();
