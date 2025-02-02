f03a2746c006a563f57d0f115501a159b090917b
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-02-02 18:18:30 +0500

    JsActivity1-Q120

--- index120.js
+++ index120.js
@@ -7,7 +7,4 @@ console.log(false || false); // false
 // This means that the OR operator is evaluated after the AND operator.
 // For example:
 console.log(true && false || true); // true
-// The above expression is evaluated as follows:
-// 1. true && false = false
-// 2. false || true = true
-// Therefore, the final result is true.
+
