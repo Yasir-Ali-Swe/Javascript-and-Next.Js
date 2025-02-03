391465d63b1f6106d2321e18ffb07a2f9cca6ec2
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-02-03 21:00:34 +0500

    JsActivity1-Q120

--- index120.js
+++ index120.js
@@ -1,10 +1,8 @@
-// Short Circuits
+
 console.log(true && true); // true
 console.log(true && false); // false
 console.log(true || false); // true
 console.log(false || false); // false
-// The OR operator has a lower precedence than the AND operator.
-// This means that the OR operator is evaluated after the AND operator.
-// For example:
+
 console.log(true && false || true); // true
 
